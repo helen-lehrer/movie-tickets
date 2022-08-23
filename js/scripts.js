59745b27b67ac191b4d0112b953f8dfb15878506
Helen Lehrer <hlehrer@outlook.com>
2022-08-23 16:11:44 -0700

    Add handleNewAccountSubmission() function
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function BankAccount(name, balance) {
   this.name = name;
   this.balance = parseInt(balance);
@@ -10,3 +12,30 @@ BankAccount.prototype.deposit = function (number) {
 BankAccount.prototype.withdraw = function (number) {
   return this.balance - parseInt(number);
 }
+
+//UI Logic
+function handleNewAccountSubmission(event) {
+  event.preventDefault();
+  let name = document.getElementById("new-name").value;
+  let balance = document.getElementById("initial-deposit").value;
+  let bankAccount = new BankAccount(name, balance);
+}
+
+function handleDepositSubmission(event) {
+  event.preventDefault();
+
+}
+
+function handleWithdrawSubmission(event) {
+  event.preventDefault();
+
+}
+
+window.addEventListener("load", function() {
+  const newAccountForm = document.getElementById("new-account");
+  const depositForm = document.getElementById("deposit");
+  const withdrawForm = document.getElementById("withdraw");
+  newAccountForm.addEventListener("submit", handleNewAccountSubmission);
+ depositForm.addEventListener("submit", handleDepositSubmission);
+  withdrawForm.addEventListener("submit", handleWithdrawSubmission);
+})
